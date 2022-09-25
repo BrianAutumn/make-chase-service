457aaaab31569f05613e6b251b324c449f26cee2
BJTowne <bjtowne9@gmail.com>
2022-09-25 10:40:21 -0400

    Basic connection generation

--- src/utils/connection-generator.ts
+++ src/utils/connection-generator.ts
@@ -5,17 +5,19 @@ import {lineCircle, lineLine} from "intersects";
 
 type Connection = Array<string>;
 
-export function generateConnections(nodes:Array<BoardNode>, connections:number, nodeRadius, nodeInfluenceMultiplier):Array<Connection>{
+export function generateRandomConnections(nodes:Array<BoardNode>, connections:number, nodeRadius, nodeInfluenceModifier = 2):Array<Connection>{
   let nodeMap = {};
   nodes.forEach(node => nodeMap[node.label] = node);
   let combos = shuffle(getAllCombinations(nodes));
   let result = [];
   let i = 0;
   while(result.length < connections && i < combos.length){
-
-    i--
+    if(!connectionCollides(combos[i],nodeMap,result,nodeRadius  * nodeInfluenceModifier)){
+      result.push(combos[i])
+    }
+    i++
   }
-  return null;
+  return result;
 }
 
 export function getAllCombinations(nodes:Array<BoardNode>):Array<Array<string>>{
@@ -25,15 +27,28 @@ export function getAllCombinations(nodes:Array<BoardNode>):Array<Array<string>>{
 }
 
 function connectionCollides(subject:Array<string>,nodeMap:Record<string,BoardNode>,connections:Array<Array<string>>,nodeRadius){
-  let start = nodeMap[subject[0]];
-  let end = nodeMap[subject[1]];
+  // if(collidesConnections(subject,nodeMap,connections)){
+  //   return true;
+  // }
+  return collidesNodes(subject, nodeMap, nodeRadius);
+}
+
+// function collidesConnections(connection:Array<string>,nodeMap:Record<string,BoardNode>,connections:Array<Array<string>>){
+//   let start = nodeMap[connection[0]];
+//   let end = nodeMap[connection[1]]
+//   for(let connection of connections){
+//     if(connectionCollidesConnection(start,end,nodeMap[connection[0]],nodeMap[connection[1]])){
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+function collidesNodes(connection:Array<string>,nodeMap:Record<string,BoardNode>,nodeRadius:number):boolean{
+  let start = nodeMap[connection[0]];
+  let end = nodeMap[connection[1]]
   for(let node of Object.values(nodeMap)){
     if(connectionCollidesNode(start,end,node,nodeRadius)){
-      return true;
-    }
-  }
-  for(let connection of connections){
-    if(connectionCollidesConnection(start,end,nodeMap[connection[0]],nodeMap[connection[1]])){
       return true;
     }
   }
@@ -47,6 +62,6 @@ function connectionCollidesNode(connectionStart:BoardNode,connectionEnd:BoardNod
   return lineCircle(connectionStart.x, connectionStart.y, connectionEnd.x, connectionEnd.y, node.x, node.y,nodeRadius);
 }
 
-function connectionCollidesConnection(aStart:BoardNode,aEnd:BoardNode,bStart:BoardNode,bEnd:BoardNode):boolean{
-  return lineLine(aStart.x, aStart.y, aEnd.x, aEnd.y, bStart.x, bStart.y, bEnd.x, bEnd.y, 1, 1);
-}
+// function connectionCollidesConnection(aStart:BoardNode,aEnd:BoardNode,bStart:BoardNode,bEnd:BoardNode):boolean{
+//   return lineLine(aStart.x, aStart.y, aEnd.x, aEnd.y, bStart.x, bStart.y, bEnd.x, bEnd.y, 1, 1);
+// }
