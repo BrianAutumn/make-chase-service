be853a6f843851a582acb35fb8041c11440ca5de
BJTowne <bjtowne9@gmail.com>
2022-09-27 15:58:09 -0400

    Debugging

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ schema = mapSchema(schema, {
       fieldConfig.resolve = async function (source, args, context, info) {
         let session = context?.event?.headers?.cookie?.match(/(?<=session=).*?(?=$| |;)/g)[0]
         if (!session) {
-          throw new AuthenticationError('No Session')
+          throw new AuthenticationError(`No Session (${context?.event?.headers?.cookie})`)
         }
         let user;
         try {
