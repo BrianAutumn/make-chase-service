6954c7e54e96e6cbcff2be644d3ab205507c1921
BJTowne <bjtowne9@gmail.com>
2022-09-26 21:45:34 -0400

    Updated mongoose configs

--- src/mongooseManager.ts
+++ src/mongooseManager.ts
@@ -10,7 +10,7 @@ const uri = tokens.dbConn;
 
 export async function connect() {
   if (conn == null) {
-    conn = mongoose.connect(uri).then(() => mongoose);
+    conn = mongoose.connect(uri,{maxIdleTimeMS : 270000, minPoolSize : 2, maxPoolSize : 4}).then(() => mongoose);
 
     // `await`ing connection after assigning to the `conn` variable
     // to avoid multiple function calls creating new connections
