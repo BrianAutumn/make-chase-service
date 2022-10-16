9738e8df2cd13743e06b47f9148c5cf33a99bd6f
BJTowne <bjtowne9@gmail.com>
2022-10-16 19:13:18 -0400

    Added testing logs

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -27,19 +27,26 @@ export async function validateJWT(jwt: any): Promise<ValidateJWTResult> {
     console.log('payload',payload)
     let user = await UserModel.findOne({sub: payload.sub, iss: payload.iss});
     if (!user) {
+      console.log('Flag A')
       user = new UserModel();
       user.sub = payload.sub;
       user.iss = payload.iss;
       user.displayName = generateName(payload.given_name, payload.family_name)
+      console.log('Flag B')
       user.email = payload.email;
       user.created = Date.now();
+      console.log('Flag C')
       await user.save();
+      console.log('Flag D')
     }
+    console.log('Flag E')
     let sessionDetails = {
       created: Date.now(),
       id: user._id
     }
+    console.log('Flag F')
     let sessionToken = encrypt(JSON.stringify(sessionDetails));
+    console.log('Flag G')
     return {
       success: true,
       sessionToken,
